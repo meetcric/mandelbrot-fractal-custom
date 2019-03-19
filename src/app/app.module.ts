39d15130237806fcab34c5c43367d6a69d63204d
meetcric <mp32445@gmail.com>
2019-03-20 02:03:08 +0530

    velocity mode added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule,Injector } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FractalImageComponent } from './fractal-image/fractal-image.component';
 
 
 import {MatCardModule} from '@angular/material/card';
+import {MatRadioModule} from '@angular/material/radio';
 
 import { createCustomElement } from '@angular/elements';
 
@@ -17,9 +19,12 @@ import { createCustomElement } from '@angular/elements';
   ],
   imports: [
     BrowserModule,
-    MatCardModule
+    MatCardModule,
+    MatRadioModule,
+    FormsModule
   ],
   providers: [],
+  // bootstrap: [AppComponent]
   entryComponents : [FractalImageComponent]
 })
 export class AppModule {
